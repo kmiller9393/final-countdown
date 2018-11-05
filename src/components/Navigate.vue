098a206f4bf3c07ad179168fcc94c733022f2921
Kurt Miller <kmiller9393@gmail.com>
2018-11-05 16:47:51 -0700

    Add Navigate component

---
+++ src/components/Navigate.vue
@@ -0,0 +1,16 @@
+<template>
+   <div class="navigate">
+     <button v-on:click="navigate()">Previous</button>
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'navigate',
+  methods: {
+    navigate() {
+      console.log('navigate');
+    }
+  }
+};
+</script>
