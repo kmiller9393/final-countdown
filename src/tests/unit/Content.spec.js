1f77f9359e54e0a2cec2b0bd5035ca0bcf5d9bd1
Kurt Miller <kmiller9393@gmail.com>
2018-11-06 16:18:11 -0700

    Update Content testing

--- src/tests/unit/Content.spec.js
+++ src/tests/unit/Content.spec.js
@@ -1,9 +1,9 @@
 import Content from '../../components/Content.vue';
-import { shallow } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 
 describe('Content.vue', () => {
   it('should match the snapshot', () => {
-    const wrapper = shallow(Content);
+    const wrapper = shallowMount(Content);
     expect(wrapper).toMatchSnapshot();
   });
 });
