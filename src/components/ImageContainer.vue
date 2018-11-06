8a6f039bc32fa7e1a702a24445985821cddc1a87
Kurt Miller <kmiller9393@gmail.com>
2018-11-05 20:03:07 -0700

    Add functionality to navigate between today's image and yesterday's in ImageContainer

--- src/components/ImageContainer.vue
+++ src/components/ImageContainer.vue
@@ -3,26 +3,45 @@
         <h1>NASA Astronomy Picture of The Day</h1>
         <div :onload="fetchPic()">
           <img :src="spaceImage" height="450" width="650" />
+          <button v-on:click="navigateBack()">Yesterday</button>
+          <button v-on:click="resetImage()">Today</button>
         </div>
     </div>
 </template>
 
 <script>
-import { fetchSpacePic } from '../utils/apiCalls';
-import key from '../assets/key';
+import key from '../assets/key/key.js';
+const currentDate;
+const previousDate;
 
 export default {
   name: 'image-container',
   data() {
     return {
-      spaceImage: ''
+      spaceImage: '',
+      images: null
     };
   },
   methods: {
-    fetchPic() {
-      fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
-        .then(response => response.json())
-        .then(spaceData => (this.spaceImage = spaceData.url));
+    async fetchPic() {
+      let url = `https://api.nasa.gov/planetary/apod?api_key=${key}&start_date=2018-11-04&end_date=2018-11-05`;
+      try {
+        const response = await fetch(url);
+        const result = await response.json();
+        this.images = result;
+        if (this.spaceImage) {
+          return;
+        }
+        this.spaceImage = result[result.length - 1].url;
+      } catch (error) {
+        return error.message;
+      }
+    },
+    navigateBack() {
+      this.spaceImage = this.images[0].url;
+    },
+    resetImage() {
+      this.spaceImage = this.images[1].url;
     }
   }
 };
